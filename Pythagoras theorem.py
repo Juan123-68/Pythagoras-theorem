ef94bae2d98aacf4b65ad30ac53dbcfd3eb6425e
Juan123-68 <j7793528@gmail.com>
2024-11-17 17:03:44 +0100

    Add files via upload

---
+++ Pythagoras theorem.py
@@ -0,0 +1,13 @@
+
+first_number = input("Inserte el primer número: ")
+second_number = input("Inserte el segundo número: ")
+
+
+from math import sqrt # sqrt significa raíz cuadrada
+
+
+def pythagoras_theorem(number_one, number_two):
+    result = number_one ** 2 + number_two ** 2
+    print(sqrt(result))
+    
+pythagoras_theorem(int(first_number), int(second_number))
