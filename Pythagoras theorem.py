4bfffef2ee347b7baf2b44bda86734862631cebb
Juan123-68 <j7793528@gmail.com>
2024-12-03 19:31:32 +0100

    Update Pythagoras theorem.py

--- Pythagoras theorem.py
+++ Pythagoras theorem.py
@@ -1,13 +1,9 @@
+from math import sqrt
 
 first_number = input("Inserte el primer número: ")
 second_number = input("Inserte el segundo número: ")
 
-
-from math import sqrt
-
-
 def pythagoras_theorem(number_one, number_two):
-    result = number_one ** 2 + number_two ** 2
-    print(sqrt(result))
+    return (number_one ** 2 + number_two ** 2) / 3
     
-pythagoras_theorem(int(first_number), int(second_number))
+print(pythagoras_theorem(int(first_number), int(second_number)))
